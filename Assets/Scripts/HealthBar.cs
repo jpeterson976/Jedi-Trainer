0140c630d17728fdf6f4ea53cd47c7cfd88c8d56
Jacob <jpeterson976@gmail.com>
2021-02-06 16:51:10 -0500

    healthbar setup

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetHealth(int health) => slider.value = health;
+}
---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    public int maxHealth;
+    private int currentHealth;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetHealth(maxHealth);
+    }
+
+    public void Heal()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetHealth(currentHealth);
+    }
+
+    public void Damage(int dmg)
+    {
+        currentHealth -= dmg;
+        healthBar.SetHealth(currentHealth);
+    }
+
+    public bool isDead()
+    {
+        return currentHealth <= 0;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Damage(10);
+        }
+        else if (Input.GetButtonDown("Horizontal"))
+        {
+            Heal();
+        }
+
+        if (isDead())
+        {
+            Debug.Log("You Died");
+        }
+    }
+}
