1796c12d25fc59e20d96b2a4fb496fa96e339dc7
roughdraftofme <45472736+roughdraftofme@users.noreply.github.com>
2021-02-07 00:17:20 -0500

    made blaster things happen

---
+++ Assets/Scripts/ShootBlaster.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBlaster : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject blasterBolt;
+
+    private int counter = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.gameObject.transform.LookAt(player.transform);
+
+        counter++;
+
+        if (counter > 100)
+        {
+            GameObject.Instantiate(blasterBolt, this.transform.position, this.transform.rotation);
+            this.gameObject.GetComponent<AudioSource>().Play();
+            counter = 0;
+        }
+    }
+}
