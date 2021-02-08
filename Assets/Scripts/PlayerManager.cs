bfecf41db222e83fe5bf693b8b1e88181e9cd214
Jacob <jpeterson976@gmail.com>
2021-02-07 22:04:25 -0500

    force push added

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -8,6 +8,10 @@ public class PlayerManager : MonoBehaviour
     public SteamVR_TrackedController left;
     public SteamVR_TrackedController right;
 
+    public int pushForce;
+    public float pushCooldown;
+    private float pushTimer;
+
     void Start()
     {
         
@@ -15,9 +19,30 @@ public class PlayerManager : MonoBehaviour
 
     void Update()
     {
+        if (pushTimer > 0)
+        {
+            pushTimer -= Time.deltaTime;
+        }
+
         if (right.gripped)
         {
             health.Heal();
+        }
+        else if (left.padPressed && pushTimer <= 0)
+        {
+            ForcePush();
+            pushTimer = pushCooldown;
+        }
+    }
+
+    void ForcePush()
+    {
+        GameObject[] droids = GameObject.FindGameObjectsWithTag("Droid");
+
+        foreach (GameObject droid in droids)
+        {
+            Rigidbody rb = droid.GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * pushForce, ForceMode.Impulse);
         }
     }
 }
