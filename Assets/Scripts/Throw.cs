927a848df3fa9156b8c53895dabdbdf6bbca793b
Jacob <jpeterson976@gmail.com>
2021-02-12 20:08:10 -0500

    force throw stuffs

---
+++ Assets/Scripts/Throw.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour
+{
+    private SteamVR_TrackedController left;
+
+    void Start()
+    {
+        left = GetComponent<SteamVR_TrackedController>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject obj = other.gameObject;
+
+        if (obj.tag.Equals("Throwable"))
+        {
+            obj.GetComponent<Throwable>().Select();
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        GameObject obj = other.gameObject;
+
+        if (obj.tag.Equals("Throwable"))
+        {
+            if (left.gripped)
+                obj.GetComponent<Throwable>().Yeet();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        GameObject obj = other.gameObject;
+
+        if (obj.tag.Equals("Throwable"))
+        {
+            obj.GetComponent<Throwable>().Deselect();
+        }
+    }
+}
---
+++ Assets/Scripts/Throwable.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwable : MonoBehaviour
+{
+    public GameObject highlight;
+
+    private float throwForce = 2.0f;
+    private Rigidbody rb;
+    private bool thrown = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        highlight.SetActive(false);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Select()
+    {
+        highlight.SetActive(true);
+    }
+
+    public void Deselect()
+    {
+        highlight.SetActive(false);
+    }
+
+    public void Yeet()
+    {
+        if (thrown)
+            return;
+
+        thrown = true;
+        GameObject droid = GameObject.FindWithTag("Droid");
+        Vector3 throwDir = droid.transform.position - this.transform.position;
+        throwDir.y += 5;
+
+        rb.AddForce(throwDir * throwForce, ForceMode.Impulse);
+    }
+}
